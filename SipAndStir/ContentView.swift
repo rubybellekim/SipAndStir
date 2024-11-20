67d6a5427bb60353a9f867796394529e0cf35a8f
rubythebelle <rubybellekim@gmail.com>
2024-11-20 14:06:18 -0800

    Initial Commit

---
+++ SipAndStir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SipAndStir
+//
+//  Created by Ruby Kim on 2024-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SipAndStir/SipAndStirApp.swift
@@ -0,0 +1,17 @@
+//
+//  SipAndStirApp.swift
+//  SipAndStir
+//
+//  Created by Ruby Kim on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct SipAndStirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
